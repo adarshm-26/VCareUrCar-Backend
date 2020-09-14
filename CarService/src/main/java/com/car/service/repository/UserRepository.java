9426fd693c09d972c18044a61c83b10729f333ee
ajay <ajayaj8919@gmail.com>
2020-09-14 08:12:32 +0530

    old repo removed

--- CarService/src/main/java/com/car/service/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.car.service.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.car.service.model.User;
-
-public interface UserRepository extends MongoRepository<User,Integer>{
-	public User findByEmail(String email);
-}
